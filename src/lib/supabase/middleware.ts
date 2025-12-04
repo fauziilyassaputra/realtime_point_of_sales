91a8dfbcdcdac327440f424275d60d064b7bf45e
fauzi <fauziilyassaputra@gmail.com>
2025-12-04 19:03:15 +0700

    repait bug from middleware

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -13,7 +13,9 @@ export async function updateSession(request: NextRequest) {
         return request.cookies.getAll();
       },
       setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value }) => cookieStore.set(name, value));
+        cookiesToSet.forEach(({ name, value }) =>
+          request.cookies.set(name, value)
+        );
         supabaseResponse = NextResponse.next({ request });
         cookiesToSet.forEach(({ name, value, options }) =>
           supabaseResponse.cookies.set(name, value, options)
@@ -21,4 +23,24 @@ export async function updateSession(request: NextRequest) {
       },
     },
   });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // jika belum login (!user) dan tidak dihalaman login
+  if (!user && !request.nextUrl.pathname.startsWith("/login")) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  // jika sudah login
+  if (user && request.nextUrl.pathname.startsWith("login")) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/";
+    return NextResponse.redirect(url);
+  }
+
+  return supabaseResponse;
 }
