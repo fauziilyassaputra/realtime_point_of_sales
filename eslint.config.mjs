89fec2bc2c47eed219af110c210b858266050399
fauzi <fauziilyassaputra@gmail.com>
2025-12-04 14:51:09 +0700

    create darkmode and develop backend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,14 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+// menonaktifkan hal-hal di eslintconfig
+eslintConfig.push({
+  rules: {
+    "react-hooks/exhaustive-deps": "off", // melewatkan variable dalam depedensi array
+    "react/jsx-key": "off", // tidak butuh key
+    "@typescript-eslint/no-explicit-any": "off", // boleh menggunakan any
+    "@typescript-eslint/no-unused-vars": "off", // boleh ada variable yang tidak digunakan
+  },
+});
+
 export default eslintConfig;
