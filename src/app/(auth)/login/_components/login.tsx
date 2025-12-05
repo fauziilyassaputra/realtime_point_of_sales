9ebc3ac0ea6e727d56985b40b2bb947d9a8b9df9
fauzi <fauziilyassaputra@gmail.com>
2025-12-05 07:10:59 +0700

    update reacthookform

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -1,17 +1,83 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { INITIAL_LOGIN_FORM } from "@/constants/auth-constant";
+import { LoginForm, loginSchema } from "@/validations/auth-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: INITIAL_LOGIN_FORM,
+  });
+  const onSubmit = form.handleSubmit(async (data) => {
+    console.log(data);
+  });
   return (
     <Card>
       <CardHeader className="text-center">
         <CardTitle className="text-xl">Welcome</CardTitle>
         <CardDescription>Login to access all features </CardDescription>
       </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={onSubmit}>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field: { ...rest } }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...rest}
+                      type="email"
+                      placeholder="Indert your email"
+                      autoComplete="off"
+                    ></Input>
+                  </FormControl>
+                  <FormMessage className="text-xs"></FormMessage>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field: { ...rest } }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...rest}
+                      type="password"
+                      placeholder="*******"
+                      autoComplete="off"
+                    ></Input>
+                  </FormControl>
+                  <FormMessage className="text-xs"></FormMessage>
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </CardContent>
     </Card>
   );
 }
