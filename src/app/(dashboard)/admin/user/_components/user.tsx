30f40f1306d089cfaf451878a1d4d62fc3f28490
fauzi <fauziilyassaputra@gmail.com>
2025-12-21 14:53:50 +0700

    add pagination and bounce

--- src/app/(dashboard)/admin/user/_components/user.tsx
+++ src/app/(dashboard)/admin/user/_components/user.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { HEADER_TABLE_USER } from "@/constants/user-constant";
+import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { useQuery } from "@tanstack/react-query";
 import { Pencil, Trash2 } from "lucide-react";
@@ -13,24 +14,34 @@ import { toast } from "sonner";
 
 export default function UserManagement() {
   const supabase = createClient();
+  const {
+    currentPage,
+    currentLimit,
+    currentSearch,
+    handleChangePage,
+    handleChangeLimit,
+    handleChangeSearch,
+  } = useDataTable();
   const { data: users, isLoading } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["users", currentPage, currentLimit, currentSearch],
     queryFn: async () => {
-      const { data, error } = await supabase
+      const result = await supabase
         .from("profiles")
         .select("*", { count: "exact" })
-        .order("created_at");
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
+        .order("created_at")
+        .ilike("name", `%${currentSearch}`);
 
-      if (error)
+      if (result.error)
         toast.error("Get user data failed", {
-          description: error.message,
+          description: result.error.message,
         });
-      return data;
+      return result;
     },
   });
 
   const filteredData = useMemo(() => {
-    return (users || []).map((user, index) => {
+    return (users?.data || []).map((user, index) => {
       return [
         index + 1,
         user.id,
@@ -63,12 +74,21 @@ export default function UserManagement() {
     });
   }, [users]);
 
+  const totalPages = useMemo(() => {
+    return users && users.count !== null
+      ? Math.ceil(users.count / currentLimit)
+      : 0;
+  }, [users]);
+
   return (
     <div className="w-full">
       <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
         <h1 className="text-2xl font-bold">User management</h1>
         <div className="flex gap-2">
-          <Input placeholder="Search by name" />
+          <Input
+            placeholder="Search by name"
+            onChange={(e) => handleChangeSearch(e.target.value)}
+          />
           <Dialog>
             <DialogTrigger asChild>
               <Button variant="outline">Create</Button>
@@ -80,6 +100,11 @@ export default function UserManagement() {
         header={HEADER_TABLE_USER}
         data={filteredData}
         isLoading={isLoading}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
       />
     </div>
   );
---
+++ src/hooks/use-data-table.tsx
@@ -0,0 +1,36 @@
+import { DEFAULT_LIMIT, DEFAULT_PAGE } from "@/constants/data-table-constant";
+import { Search } from "lucide-react";
+import { useState } from "react";
+import useDebounce from "./use-debounce";
+
+export default function useDataTable() {
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
+  const [currentLimit, setCurrentLimit] = useState(DEFAULT_LIMIT);
+  const [currentSearch, setCurrentSearch] = useState("");
+  const debounce = useDebounce();
+
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handleChangeLimit = (page: number) => {
+    setCurrentLimit(page);
+    setCurrentPage(DEFAULT_PAGE);
+  };
+
+  const handleChangeSearch = (Search: string) => {
+    debounce(() => {
+      setCurrentSearch(Search);
+      setCurrentPage(DEFAULT_PAGE);
+    }, 500);
+  };
+
+  return {
+    currentPage,
+    handleChangePage,
+    currentLimit,
+    handleChangeLimit,
+    currentSearch,
+    handleChangeSearch,
+  };
+}
