6f907fe0e1a6cd84c64f475aa52178cc3a9c303c
fauzi <fauziilyassaputra@gmail.com>
2025-12-25 22:58:47 +0700

    create menus in migration

---
+++ src/app/migrations/002-menu.sql
@@ -0,0 +1,17 @@
+create table public.menus (
+  id serial not null,
+  name text,
+  variant text,
+  price numeric,
+  discount numeric,
+  image_url text,
+  category text,
+  is_availabel boolean,
+  created_at timestamp with time zone default timezone('utc'::text, now()) not null,
+  updated_at timestamp with time zone default timezone('utc'::text, now()) not null,
+
+  primary key (id)
+);
+
+
+alter table public.menus enable row level security;
