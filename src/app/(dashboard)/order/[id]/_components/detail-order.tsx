789de11deb816fe0269343423491da44a6b2c53d
fauzi <fauziilyassaputra@gmail.com>
2026-01-05 18:14:36 +0700

    fix bug in order

--- src/app/(dashboard)/order/[id]/_components/detail-order.tsx
+++ src/app/(dashboard)/order/[id]/_components/detail-order.tsx
@@ -1,3 +1,237 @@
+// "use client";
+
+// import DataTable from "@/components/common/data-table";
+// import { Button } from "@/components/ui/button";
+// import { HEADER_TABLE_DETAIL_ORDER } from "@/constants/order-constant";
+// import useDataTable from "@/hooks/use-data-table";
+// import { createClientSupabase } from "@/lib/supabase/default";
+// import { cn, convertIDR } from "@/lib/utils";
+// import { useQuery } from "@tanstack/react-query";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { startTransition, useActionState, useEffect, useMemo } from "react";
+// import { toast } from "sonner";
+// import Summary from "./summary";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
+// import { EllipsisVertical } from "lucide-react";
+// import { updateStatusOrderItem } from "../../actions";
+// import { INITIAL_STATE_ACTION } from "@/constants/general-constant";
+// import { useAuthStore } from "@/stores/auth-store";
+
+// export default function DetailOrder({ id }: { id: string }) {
+//   const supabase = createClientSupabase();
+//   const { currentPage, currentLimit, handleChangePage, handleChangeLimit } =
+//     useDataTable();
+//   const profile = useAuthStore((state) => state.profile);
+//   const { data: order, isLoading } = useQuery({
+//     queryKey: ["order", id],
+//     queryFn: async () => {
+//       const result = await supabase
+//         .from("orders")
+//         .select("id,  customer_name, status, payment_token, tables (name, id)")
+//         .eq("order_id", id)
+//         .single();
+
+//       if (result.error)
+//         toast.error("Get Order data failed", {
+//           description: result.error.message,
+//         });
+
+//       return result.data;
+//     },
+//     enabled: !!id,
+//   });
+
+//   useEffect(() => {
+//     if (!order?.id) return;
+
+//     const channel = supabase
+//       .channel("change-order")
+//       .on(
+//         "postgres_changes",
+//         {
+//           event: "*",
+//           schema: "public",
+//           table: "orders_menus",
+//           filter: `order_id=eq.${order.id}`,
+//         },
+//         () => {
+//           refetchOrderMenu();
+//         }
+//       )
+//       .subscribe();
+
+//     return () => {
+//       supabase.removeChannel(channel);
+//     };
+//   }, [order?.id]);
+
+//   const {
+//     data: orderMenu,
+//     isLoading: isLoadingOrderMenu,
+//     refetch: refetchOrderMenu,
+//   } = useQuery({
+//     queryKey: ["orders_menu", order?.id, currentPage, currentLimit],
+//     queryFn: async () => {
+//       const result = await supabase
+//         .from("orders_menus")
+//         .select("*, menus(id, name, image_url, price)", { count: "exact" })
+//         .eq("order_id", order?.id)
+//         .order("status");
+
+//       if (result.error)
+//         toast.error("Get Order menu data failed", {
+//           description: result.error.message,
+//         });
+
+//       return result;
+//     },
+//     enabled: !!order?.id,
+//   });
+
+//   const [updateStatusOrderState, updateStatusOrderAction] = useActionState(
+//     updateStatusOrderItem,
+//     INITIAL_STATE_ACTION
+//   );
+
+//   const handleUpdateStatusOrder = async (data: {
+//     id: string;
+//     status: string;
+//   }) => {
+//     const formData = new FormData();
+//     Object.entries(data).forEach(([keyof, value]) => {
+//       formData.append(keyof, value);
+//     });
+//     startTransition(() => {
+//       updateStatusOrderAction(formData);
+//     });
+//   };
+
+//   useEffect(() => {
+//     if (updateStatusOrderState?.status === "error") {
+//       toast.error("Create Status Order Failed ", {
+//         description: updateStatusOrderState.errors?._form?.[0],
+//       });
+//     }
+
+//     if (updateStatusOrderState?.status === "success") {
+//       toast.success("Create Status Order Success");
+//     }
+//   }, [updateStatusOrderState]);
+
+//   const filteredData = useMemo(() => {
+//     return (orderMenu?.data || []).map((item, index) => {
+//       return [
+//         currentLimit * (currentPage - 1) + index + 1,
+//         <div className="flex items-center gap-2">
+//           <Image
+//             src={item.menus.image_url}
+//             alt={item.menus.name}
+//             width={40}
+//             height={40}
+//             className="rounded"
+//           />
+//           <div className="flex flex-col">
+//             {item.menus.name} x {item.quantity}
+//             <span className="text-xs text-muted-foreground">
+//               {item.notes || "No Notes"}
+//             </span>
+//           </div>
+//         </div>,
+//         <div className="">{convertIDR(item.menus.price * item.quantity)}</div>,
+//         <div
+//           className={cn("px-2 py-1 rounded-full text-white w-fit capitalize", {
+//             "bg-gray-500": item.status === "pending",
+//             "bg-yellow-500": item.status === "process",
+//             "bg-blue-500": item.status === "ready",
+//             "bg-green-500": item.status === "served",
+//           })}
+//         >
+//           {item.status}
+//         </div>,
+//         <DropdownMenu>
+//           <DropdownMenuTrigger asChild>
+//             <Button
+//               variant="ghost"
+//               className={cn(
+//                 "data-[state=open]:bg-muted text-muted-foreground flex size-8",
+//                 { hidden: item.status === "served" }
+//               )}
+//               size="icon"
+//             >
+//               <EllipsisVertical />
+//             </Button>
+//           </DropdownMenuTrigger>
+//           <DropdownMenuContent align="end" className="w-40">
+//             {["pending", "process", "ready"].map((status, index) => {
+//               const nextStatus = ["process", "ready", "served"][index];
+//               return (
+//                 item.status === status && (
+//                   <DropdownMenuItem
+//                     className="capitalize"
+//                     key={status}
+//                     onClick={() =>
+//                       handleUpdateStatusOrder({
+//                         id: item.id,
+//                         status: nextStatus,
+//                       })
+//                     }
+//                   >
+//                     {nextStatus}
+//                   </DropdownMenuItem>
+//                 )
+//               );
+//             })}
+//           </DropdownMenuContent>
+//         </DropdownMenu>,
+//       ];
+//     });
+//   }, [orderMenu?.data]);
+
+//   const totalPages = useMemo(() => {
+//     return orderMenu && orderMenu.count !== null
+//       ? Math.ceil(orderMenu.count / currentLimit)
+//       : 0;
+//   }, [orderMenu]);
+
+//   return (
+//     <div className="w-full space-y-4">
+//       <div className="flex  items-center justify-between gap-4 w-full">
+//         <h1 className="text-2xl font-bold">Detail Order</h1>
+//         {profile.role !== "kitchen" && (
+//           <Link href={`/order/${id}/add`}>
+//             <Button>Add Order Item</Button>
+//           </Link>
+//         )}
+//       </div>
+//       <div className="flex flex-col lg:flex-row gap-4 w-full">
+//         <div className="lg:w-2/3">
+//           <DataTable
+//             header={HEADER_TABLE_DETAIL_ORDER}
+//             data={filteredData}
+//             isLoading={isLoadingOrderMenu}
+//             totalPages={totalPages}
+//             currentPage={currentPage}
+//             currentLimit={currentLimit}
+//             onChangePage={handleChangePage}
+//             onChangeLimit={handleChangeLimit}
+//           />
+//         </div>
+//         <div className="lg:w-1/3">
+//           {order && (
+//             <Summary order={order} orderMenu={orderMenu?.data} id={id} />
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import DataTable from "@/components/common/data-table";
@@ -28,12 +262,13 @@ export default function DetailOrder({ id }: { id: string }) {
   const { currentPage, currentLimit, handleChangePage, handleChangeLimit } =
     useDataTable();
   const profile = useAuthStore((state) => state.profile);
-  const { data: order, isLoading } = useQuery({
+
+  const { data: order } = useQuery({
     queryKey: ["order", id],
     queryFn: async () => {
       const result = await supabase
         .from("orders")
-        .select("id,  customer_name, status, payment_token, tables (name, id)")
+        .select("id, customer_name, status, payment_token, tables (name, id)")
         .eq("order_id", id)
         .single();
 
@@ -45,29 +280,6 @@ export default function DetailOrder({ id }: { id: string }) {
       return result.data;
     },
     enabled: !!id,
-  });
-
-  const {
-    data: orderMenu,
-    isLoading: isLoadingOrderMenu,
-    refetch: refetchOrderMenu,
-  } = useQuery({
-    queryKey: ["orders_menu", order?.id, currentPage, currentLimit],
-    queryFn: async () => {
-      const result = await supabase
-        .from("orders_menus")
-        .select("*, menus(id, name, image_url, price)", { count: "exact" })
-        .eq("order_id", order?.id)
-        .order("status");
-
-      if (result.error)
-        toast.error("Get Order menu data failed", {
-          description: result.error.message,
-        });
-
-      return result;
-    },
-    enabled: !!order?.id,
   });
 
   useEffect(() => {
@@ -94,6 +306,29 @@ export default function DetailOrder({ id }: { id: string }) {
     };
   }, [order?.id]);
 
+  const {
+    data: orderMenu,
+    isLoading: isLoadingOrderMenu,
+    refetch: refetchOrderMenu,
+  } = useQuery({
+    queryKey: ["orders_menu", order?.id, currentPage, currentLimit],
+    queryFn: async () => {
+      const result = await supabase
+        .from("orders_menus")
+        .select("*, menus (id, name, image_url, price)", { count: "exact" })
+        .eq("order_id", order?.id)
+        .order("status");
+
+      if (result.error)
+        toast.error("Get order menu data failed", {
+          description: result.error.message,
+        });
+
+      return result;
+    },
+    enabled: !!order?.id,
+  });
+
   const [updateStatusOrderState, updateStatusOrderAction] = useActionState(
     updateStatusOrderItem,
     INITIAL_STATE_ACTION
@@ -104,9 +339,10 @@ export default function DetailOrder({ id }: { id: string }) {
     status: string;
   }) => {
     const formData = new FormData();
-    Object.entries(data).forEach(([keyof, value]) => {
-      formData.append(keyof, value);
+    Object.entries(data).forEach(([Key, value]) => {
+      formData.append(Key, value);
     });
+
     startTransition(() => {
       updateStatusOrderAction(formData);
     });
@@ -114,13 +350,13 @@ export default function DetailOrder({ id }: { id: string }) {
 
   useEffect(() => {
     if (updateStatusOrderState?.status === "error") {
-      toast.error("Create Status Order Failed ", {
+      toast.error("Update Status Order Failed", {
         description: updateStatusOrderState.errors?._form?.[0],
       });
     }
 
     if (updateStatusOrderState?.status === "success") {
-      toast.success("Create Status Order Success");
+      toast.success("Update Status Order Success");
     }
   }, [updateStatusOrderState]);
 
@@ -143,7 +379,7 @@ export default function DetailOrder({ id }: { id: string }) {
             </span>
           </div>
         </div>,
-        <div className="">{convertIDR(item.menus.price * item.quantity)}</div>,
+        <div>{convertIDR(item.menus.price * item.quantity)}</div>,
         <div
           className={cn("px-2 py-1 rounded-full text-white w-fit capitalize", {
             "bg-gray-500": item.status === "pending",
@@ -173,7 +409,6 @@ export default function DetailOrder({ id }: { id: string }) {
               return (
                 item.status === status && (
                   <DropdownMenuItem
-                    className="capitalize"
                     key={status}
                     onClick={() =>
                       handleUpdateStatusOrder({
@@ -181,6 +416,7 @@ export default function DetailOrder({ id }: { id: string }) {
                         status: nextStatus,
                       })
                     }
+                    className="capitalize"
                   >
                     {nextStatus}
                   </DropdownMenuItem>
@@ -201,7 +437,7 @@ export default function DetailOrder({ id }: { id: string }) {
 
   return (
     <div className="w-full space-y-4">
-      <div className="flex  items-center justify-between gap-4 w-full">
+      <div className="flex items-center justify-between gap-4 w-full">
         <h1 className="text-2xl font-bold">Detail Order</h1>
         {profile.role !== "kitchen" && (
           <Link href={`/order/${id}/add`}>
